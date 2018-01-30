459094c1fbc79f1352265a088fdcc7cd533e1e7c
emanuelecomp <cardgameheritage@gmail.com>
2018-01-29 21:32:48 -0300

    melhorando ux

--- client/app/app.routes.ts
+++ client/app/app.routes.ts
@@ -4,7 +4,8 @@ import { CadastroComponent } from './cadastro/cadastro.component';
 
 const appRoutes: Routes  = [
   { path: '', component: ListagemComponent },
-  { path: 'cadastro', component: CadastroComponent }
+  { path: 'cadastro', component: CadastroComponent },
+  { path: '**', component: ListagemComponent }
 ];
 
 export const routing = RouterModule.forRoot(appRoutes);
